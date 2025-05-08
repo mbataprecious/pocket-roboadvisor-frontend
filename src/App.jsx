0064554e3708009382403cab3b69b9e9a9c22165
Precious Mbata <47004351+mbataprecious@users.noreply.github.com>
2025-05-09 00:52:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import DetailSection from "./components/DetailSection";
 import Footer from "./components/Footer";
-//stying
-import "./App.css";
 import CallToAction from "./components/CallToAction";
 import AboutSection from "./components/AboutSection";
 import ProfileCalculatorSection from "./components/ProfileCalculatorSection";
+
+//stying
+import "./App.css";
 
 function App() {
   return (
